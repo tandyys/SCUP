980352fb0f9d8a7e62b5a98ec59173b51e9c2e8a
Tandy Sabana <tansabana12@gmail.com>
2024-04-23 12:25:20 +0700

    Initial Commit

---
+++ SCUP/SCUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCUPApp.swift
+//  SCUP
+//
+//  Created by tandyys on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SCUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

9816a775c3f78056a266839931251c835f950d06
Vincent <31364417+vcsrng@users.noreply.github.com>
2024-05-02 10:43:09 +0700

    Last finishing (fix tutorial view ga kepotong), wp guys 👏🏻

--- SCUP/View/Presenter/TutorialView.swift
+++ SCUP/View/Presenter/TutorialView.swift
@@ -22,7 +22,7 @@ struct VideoPlayerWrapper: UIViewControllerRepresentable {
 
 struct TutorialView: View {
     var body: some View {
-        VideoPlayerWrapper(player: AVPlayer(url: URL(string: "linknya taro sini")!))
+        VideoPlayerWrapper(player: AVPlayer(url: URL(string: "https://firebasestorage.googleapis.com/v0/b/scup-8674e.appspot.com/o/videos%2FTutorialVidFix.mp4?alt=media&token=1a802011-5e39-4a90-9514-4883acc70da6")!))
             .edgesIgnoringSafeArea(.all)
     }
 }
